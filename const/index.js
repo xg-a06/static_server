baacb49353c04196fd81a0cd4f5425a6d8d090f6
xg-a06 <xg_jser@163.com>
2019-05-10 17:09:07 +0800

    更新子进程后台启动支持

---
+++ const/index.js
@@ -0,0 +1,11 @@
+const MSG_TYPE = {
+  REGISTER: 'register',
+  KILL: 'kill',
+  CLOSE: 'close',
+  EXIST: 'exist',
+  SUCCESS: 'success',
+  LIST: 'list',
+  KILL: 'kill'
+}
+
+module.exports = MSG_TYPE
